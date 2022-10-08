f633cd311c047e763aad8256bc9a8f270948f5a1
Alessandra <carvalho.machado.alessandra@gmail.com>
2022-10-08 11:50:25 -0300

    Ajuste class MainActivity

--- app/src/main/java/com/mobile/androidapp/view/MainActivity.java
+++ app/src/main/java/com/mobile/androidapp/view/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // chama activity main
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("TAG", "onCreate: antes do getInstance");
         UserRepository.getInstance(this);
         Log.e("TAG", "onCreate: depois do getInstance " + UserRepository.getInstance(this).getUsers().size());
+
 
         setContentView(R.layout.activity_main);
     }
